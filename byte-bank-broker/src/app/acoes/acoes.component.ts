6a14554595d660ab32622b7ff3285ab373876091
Daniel Bou de Souza <daniel.bou@nava.com.br>
2022-12-02 21:32:24 -0300

    Operadores tap e map

--- byte-bank-broker/src/app/acoes/acoes.component.ts
+++ byte-bank-broker/src/app/acoes/acoes.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { AcoesService } from './acoes.service';
+import { Acoes } from './modelo/acoes';
+import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
 @Component({
@@ -6,8 +8,17 @@ import { FormControl } from '@angular/forms';
   templateUrl: './acoes.component.html',
   styleUrls: ['./acoes.component.css'],
 })
-export class AcoesComponent {
-  acoesInput = new FormControl();
+export class AcoesComponent implements OnInit {
 
-  constructor() {}
+  acoesInput = new FormControl();
+  acoes: Acoes;
+
+  constructor(private acoesService: AcoesService) {}
+
+  ngOnInit(): void {
+    this.acoesService.getAcoes().subscribe(
+      (retornoApi) => this.acoes = retornoApi
+    );
+  }
+
 }
